300759ee48de236f1a85c76ce9ac4c18575b2638
galia <galiat@gmail.com>
2014-04-17 19:04:06 -0400

    spacing. 2 spaces

--- Space.js
+++ Space.js
@@ -1,23 +1,23 @@
 function Space(dom, size) {
-	this._dom = dom ? dom : document.getElementById('space');
-	this._size = size ? size : this._dom.offsetWidth;
-	this._width = this._size;
-	this._height = this._size;
+  this._dom = dom ? dom : document.getElementById('space');
+  this._size = size ? size : this._dom.offsetWidth;
+  this._width = this._size;
+  this._height = this._size;
 
-	this._dom.style.width = this._width;
-	this._dom.style.height = this._height;
+  this._dom.style.width = this._width;
+  this._dom.style.height = this._height;
 
-	this._blobs = []; // We'll use this array to store all the blobs we have
+  this._blobs = []; // We'll use this array to store all the blobs we have
 }
 
 Space.prototype.addBlob = function(blob) {
-	this._blobs.push(blob);
-	this._dom.appendChild(blob._dom);
+  this._blobs.push(blob);
+  this._dom.appendChild(blob._dom);
 
-	return blob;
+  return blob;
 };
 
 Space.prototype.removeBlob = function(blob) {
-	this._dom.removeChild(blob._dom); // remove the blob from our DOM
-	this._blobs.splice(this.blobs.indexOf(blob), 1); // & then stop tracking it
+  this._dom.removeChild(blob._dom); // remove the blob from our DOM
+  this._blobs.splice(this.blobs.indexOf(blob), 1); // & then stop tracking it
 };
